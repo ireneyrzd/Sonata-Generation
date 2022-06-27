37c3208c6d44052c405e2db809edd3a892b5b310
ireneyrzd <85361478+ireneyrzd@users.noreply.github.com>
2022-06-26 23:28:36 -0700

    split rhythm transition matrix for seperate voice

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Sonata-Generation
+Generation of Beethoven-style sonanta using Wave Function Collapse
+
+Research project at UCSC Summer Internship Program. 
+Mentor: Cassandra Ravenbrookm
+Teammate: Matthew Chen
+Dataset from: https://github.com/Tsung-Ping/functional-harmony
--- rhythm_tile_generator.py
+++ rhythm_tile_generator.py
@@ -22,9 +22,9 @@ high_voice = []
 low_voice = []
 notes_in_onset = []
 row_num = 0
-for onset in range(0, data[-1][0] + 1):
+for onset in range(0, int(data[-1][0]) + 1):
     row_num_old = row_num
-    while data[row_num][0] <= onset:
+    while row_num < len(data) and data[row_num][0] <= onset:
         notes_in_onset.append(data[row_num][1])
         row_num += 1  
     notes_in_onset.sort()
@@ -45,26 +45,31 @@ for onset in range(0, data[-1][0] + 1):
 # print('high: ')
 # print(high_voice)
 
-## !! not yet seperated for high and low voice
-
-#remove instances of stacked notes in data with equal length 
-data_short = []
-for i in range(len(data)-1):
-    if not(data[i][0] == data[i+1][0] and data[i][3] == data[i+1][3]):
-        data_short.append(data[i])
-#print(data_short)
-
-states = [] #sorted array with all possible note values
-note_val = [] #column 4 of data, sequenced note value
-for i in range(len(data)):
-    note_val.append(data[i][3])
-    if data[i][3] not in states:
-        states.append(data[i][3])
-states.sort()
-#print(states)
-#print(note_val)
 
 #create transition vectors
-transition = pd.crosstab(pd.Series(note_val[1:],name='succeeding note value'),
-                         pd.Series(note_val[:-1],name='current note value'),normalize=1)
-#print(transition)
+def create_transition(data_list):
+    #remove instances of stacked notes in data with equal length 
+    # !! needs better chord analysis
+    data_short = []
+    for i in range(len(data)-1):
+        if not(data[i][0] == data[i+1][0] and data[i][3] == data[i+1][3]):
+            data_short.append(data[i])
+    #print(data_short)
+    states = [] #sorted array with all possible note values
+    note_val = [] #column 4 of data, sequenced note value
+    for i in range(len(data)):
+        note_val.append(data[i][3])
+        if data[i][3] not in states:
+            states.append(data[i][3])
+    states.sort()
+    #print(states)
+    #print(note_val)
+    transition = pd.crosstab(pd.Series(note_val[1:],name='succeeding note value'),
+                             pd.Series(note_val[:-1],name='current note value'),normalize=1)
+    print(transition)
+
+print('high voice: ')
+create_transition(high_voice)
+print()
+print('low voice: ')
+create_transition(low_voice)
