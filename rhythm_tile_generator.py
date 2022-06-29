dee7ffe101dc0231126fa194dbdc0a0ca58fd6d1
ireneyrzd <85361478+ireneyrzd@users.noreply.github.com>
2022-06-29 12:53:16 -0700

    seperates voice and adds rests

--- rhythm_tile_generator.py
+++ rhythm_tile_generator.py
@@ -1,4 +1,3 @@
-from re import A, X
 import numpy as np
 import pandas as pd
 import csv
@@ -18,6 +17,12 @@ with open('20-notes.csv') as csv_file:
 
 #print(data)
 
+#return true if two notes are in an interval/chord
+def is_chord(a1, a2, b1, b2):
+    if not(a1 == b1 and a2 == b2):
+        return False
+    return True
+
 #split data into high (RH) and low (LH) voice
 high_voice = []
 low_voice = []
@@ -27,36 +32,76 @@ for i in range(len(data)):
     else: 
         high_voice.append(data[i])
 
-# print('low: ')
-# print(low_voice)
-# print()
+def append_rest(arr, data, rest_duration):
+    arr.append([str(data[0] + data[3]), '0.0', '0.0', str(rest_duration), str(data[4]), str(data[5]), 'R'])
+      
+#adds rests into data
+#label duration R for rest
+#label duration C for chords/interval
+#label duration N for notes
+def adds_rests(dataset, file_name):
+    data = []
+    i = 0
+    while i < len(dataset)-1:
+        #print(i)
+        if is_chord(dataset[i][0], dataset[i][3], dataset[i+1][0], dataset[i+1][3]):
+            #print('c')
+            data.append(np.append(dataset[i], 'C'))
+        elif is_chord(dataset[i-1][0], dataset[i-1][3], dataset[i][0], dataset[i][3]):
+            #print('c')
+            data.append(np.append(dataset[i], 'C'))
+        else:
+            #print('n')
+            data.append(np.append(dataset[i], 'N'))
+        rest_dur = dataset[i+1][0] - dataset[i][0] - dataset[i][3]
+        if (dataset[i+1][0] - dataset[i][0] > 0.0001 and rest_dur > 0.0001):
+            append_rest(data, dataset[i], rest_dur)
+                                      
+        i+=1
+    with open(file_name, 'w') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerows(data)
+    return data
+
+
+high_voice = adds_rests(high_voice, 'higher_voice.csv')
+low_voice = adds_rests(low_voice, 'lower_voice.csv')
+
+
 # print('high: ')
 # print(high_voice)
+# print()
+# print('low: ')
+# print(low_voice)
 
 
 #create transition vectors
-def create_transition(data_list):
+def create_transition(data_list, file_name):
     #remove instances of stacked notes in data with equal length 
     # !! needs better chord analysis
     data_short = []
     for i in range(len(data_list)-1):
-        if not(data_list[i][0] == data_list[i+1][0] and data_list[i][3] == data_list[i+1][3]):
+        if is_chord(data[i][0], data[i][3], data[i+1][0], data[i+1][3]):
             data_short.append(data_list[i])
     #print(data_short)
     states = [] #sorted array with all possible note values
     note_val = [] #column 4 of data, sequenced note value
     for i in range(len(data_short)):
-        note_val.append(data_short[i][3])
-        if data_short[i][3] not in states:
-            states.append(data_short[i][3])
+        note_val.append(data_short[i][3] + data_short[i][6])
+        if data_short[i][3] + data_short[i][6] not in states:
+            states.append(data_short[i][3] + data_short[i][6])
     states.sort()
     #print(states)
     transition = pd.crosstab(pd.Series(note_val[1:],name='succeeding note value'),
                              pd.Series(note_val[:-1],name='current note value'),normalize=1)
-    print(transition)
+
+    df = pd.DataFrame(transition)
+    df.to_csv(file_name)
+    return transition
+
 
 print('high voice: ')
-create_transition(high_voice)
+print(create_transition(high_voice, 'high_voice_transition.csv'))
 print()
 print('low voice: ')
-create_transition(low_voice)
+print(create_transition(low_voice, 'low_voice_transition.csv'))
