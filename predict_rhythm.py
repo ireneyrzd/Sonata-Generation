e04988693af2a8678d6befad305c7e108b79a524
ireneyrzd <85361478+ireneyrzd@users.noreply.github.com>
2022-07-08 22:05:50 -0700

    predict rhythm in structure

--- predict_rhythm.py
+++ predict_rhythm.py
@@ -5,15 +5,15 @@ import csv
 
 class Predict:
     # constructor
-    def __init__(self, transition_file_name, num_of_measures, start_note): #key, time signature
+    def __init__(self, transition_file_name): #key, time signature
         # initialize variables
         self.transition = []
         self.data = []
         self.states = []
-        self.num_of_measures = num_of_measures
         self.current_onset = 1
+        self.last_note = ''
+        self.first = False
         #num_of_onsets = num_of_measures * 4 #time signature
-        self.start_note = '1.0_' + start_note #starting note
         self.rhythm = [] #output rhythm
 
         #copy csv data into list data
@@ -27,47 +27,67 @@ class Predict:
         self.transition = np.delete(self.transition, 0, 1).astype(float)
 
 
-    def find_note(self):
+    def find_note(self, first):
+        self.first = first
         current_state = np.random.random()
         cumulative = 0
         for i in range(len(self.states)):
-            if self.states[i] == self.rhythm[-1]:
+            x = ''
+            if self.first:
+                x = self.last_note
+            else:
+                x = self.rhythm[-1]
+            #print('state[i]', self.states[i], 'x', x, self.states[i] == x)
+            if self.states[i] == x:
                 for j in range(1, len(self.states)):
                     cumulative += self.transition[j][i]
+                    #print('current', current_state, 'cumulative', cumulative, current_state < cumulative)
                     if current_state < cumulative:
                         return self.states[j]
-        return 'Unable to generate rhythm'        
+        return 'Unable to generate rhythm'
 
-    def predict(self):
-        for i in range(self.num_of_measures):
+    def predict(self, num_of_measures, start_note):
+        for i in range(num_of_measures):
             #print (self.rhythm)
             current_onset = 1
             if i == 0:
-                self.rhythm.append(self.start_note)
-                current = self.rhythm[-1]
+                self.rhythm.append(start_note)
+                current = ''
+                if self.first:
+                    current = self.last_note
+                else:
+                    current = self.rhythm[-1]
                 index1 = 0
                 for k in range(len(current)):
                     if current[k] == '_':
                         index1 = k+1
                         break
                 current_onset += float(current[index1:-1])
-            while current_onset < 4.98: #reduce float point error
-                self.rhythm.append(self.find_note())
+            found = False
+            while current_onset < 4.998 and not found: #reduce float point error
+                self.rhythm.append(self.find_note(False))
                 current = self.rhythm[-1]
                 index1 = 0
                 for k in range(len(current)):
                     if current[k] == '_':
                         index1 = k+1
+                        found = True
                         break
                 current_onset += float(current[index1:-1])
             #self.rhythm.append('|')
+        #print('rhythm', self.rhythm)
+        self.last_note = self.rhythm[-1]
         return self.rhythm
 
 
-print('Right hand rhythm: ')
-rh = Predict("high_voice_transition.csv", 4, '1.0N')
-print(rh.predict())
+# rh = Predict("high_voice_transition.csv")
+# rh_theme1 = rh.predict(4, '1.0N')
+# print(rh_theme1) #sentence 1
 
-print('Left hand rhythm: ')
-lh = Predict("low_voice_transition.csv", 8, '2.0C')
-print(lh.predict())
+# print('Right hand rhythm: ')
+# rh = Predict("high_voice_transition.csv", 4, '1.0N')
+# print(rh.predict())
+
+# print('Left hand rhythm: ')
+# lh = Predict("low_voice_transition.csv", 8, '2.0C')
+# print(lh.predict())
---
+++ predict_sonata.py
@@ -0,0 +1,83 @@
+from predict_rhythm import *
+import pandas as pd
+import numpy as np
+
+rh = Predict("high_voice_transition.csv")
+lh = Predict("low_voice_transition.csv",)
+
+with open('sonata.txt', 'w') as f:
+    # Exposition
+    f.write('Exposition\n')
+
+    f.write('Theme 1: \n')
+    f.write('Right hand rhythm: \n')
+    rh_theme1 = rh.predict(4, '1.0_1.0N')
+    f.write(str(rh_theme1) + '\n') #sentence 1
+    f.write(str(rh_theme1) + '\n') #setence 2
+
+    f.write('Left hand rhythm: \n')
+    lh_theme1 = lh.predict(4, '1.0_2.0C')
+    f.write(str(lh_theme1) + '\n') #sentence 1
+    f.write(str(lh_theme1) + '\n') #setence 2
+
+
+    f.write('Bridge: \n')
+    f.write('Right hand rhythm: \n')
+    rh_bridge = rh.predict(4, rh.find_note(True))
+    f.write(str(rh_bridge) + '\n') #bridge
+
+    f.write('Left hand rhythm: \n')
+    lh_bridge = lh.predict(4, lh.find_note(True))
+    f.write(str(lh_bridge) + '\n') #bridge
+
+    f.write('Theme 2 \n')
+    f.write('Right hand rhythm: \n')
+    rh_theme2 = rh.predict(4, rh.find_note(True))
+    f.write(str(rh_theme2) + '\n') #sentence 1
+    f.write(str(rh_theme2) + '\n') #setence 2
+
+    f.write('Left hand rhythm: \n')
+    lh_theme2 = lh.predict(4, lh.find_note(True))
+    f.write(str(lh_theme2) + '\n') #sentence 1
+    f.write(str(lh_theme2) + '\n') #setence 2
+
+
+    # Development
+    f.write('\nDevelopment\n')
+    f.write('Right hand rhythm: \n')
+    rh_dev = rh.predict(12, rh.find_note(True))
+    f.write(str(rh_dev) + '\n') #Development
+
+    f.write('Left hand rhythm: \n')
+    lh_dev = lh.predict(12, lh.find_note(True))
+    f.write(str(lh_dev) + '\n') #Development
+
+
+    # Recapitulation
+    f.write('\nRecapitulation\n')
+
+    f.write('Theme 1 Recap: \n')
+    f.write('Right hand rhythm: \n')
+    f.write(str(rh_theme1) + '\n') #sentence 1
+    f.write(str(rh_theme1) + '\n') #setence 2
+
+    f.write('Left hand rhythm: \n')
+    f.write(str(lh_theme1) + '\n') #sentence 1
+    f.write(str(lh_theme1) + '\n') #setence 2
+
+
+    f.write('Bridge Recap: \n')
+    f.write('Right hand rhythm: \n')
+    f.write(str(rh_bridge) + '\n') #bridge
+
+    f.write('Left hand rhythm:\n ')
+    f.write(str(lh_bridge) + '\n') #bridge
+
+    f.write('Theme 2 Recap \n')
+    f.write('Right hand rhythm: \n')
+    f.write(str(rh_theme2) + '\n') #sentence 1
+    f.write(str(rh_theme2) + '\n') #setence 2
+
+    f.write('Left hand rhythm: \n')
+    f.write(str(lh_theme2) + '\n') #sentence 1
+    f.write(str(lh_theme2) + '\n') #setence 2
